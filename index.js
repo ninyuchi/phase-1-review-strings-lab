8bb395e5f7c22d8184f3d19b68adfdf714244fd7
Nasra Kassim <ninyuchinasra@gmail.com>
2023-06-13 20:11:46 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Nasra Kassim';
+const welcomeMessage = 'Welcome to Flatbook, + ${Nasra Kassim}!'
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ${N. slice(0, 1}!';
+console.log(welcomeMessage)
